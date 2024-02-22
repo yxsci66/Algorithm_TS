0643271b063df185666c078ed0f40278573d6d40
yxsci66 <yxsci66@163.com>
2024-02-22 16:21:44 +0800

    feat add 142.linked-list-cycle-ii
    base on quick slow pointer

---
+++ src/142.linked-list-cycle-ii.ts
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=142 lang=typescript
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (50.60%)
+ * Likes:    13181
+ * Dislikes: 915
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given the head of a linked list, return the node where the cycle begins. If
+ * there is no cycle, return null.
+ *
+ * There is a cycle in a linked list if there is some node in the list that can
+ * be reached again by continuously following the next pointer. Internally, pos
+ * is used to denote the index of the node that tail's next pointer is
+ * connected to (0-indexed). It is -1 if there is no cycle. Note that pos is
+ * not passed as a parameter.
+ *
+ * Do not modify the linked list.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the list is in the range [0, 10^4].
+ * -10^5 <= Node.val <= 10^5
+ * pos is -1 or a valid index in the linked-list.
+ *
+ *
+ *
+ * Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ *
+ */
+
+import { ListNode } from "../data_structure/module";
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+export function detectCycle(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) {
+    return null;
+  }
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+  while (fast && fast.next) {
+    slow = slow!.next;
+    fast = fast.next.next;
+    if (fast === slow) {
+      break;
+    }
+  }
+  if (slow !== fast) {
+    return null;
+  }
+  slow = head;
+  while (slow !== fast) {
+    slow = slow!.next;
+    fast = fast!.next;
+  }
+  return slow;
+}
+// @lc code=end
