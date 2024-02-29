f4a308c089d8923f83decfc4b39255fc8e36a4bc
yxsci66 <yxsci66@163.com>
2024-02-29 17:31:43 +0800

    feat add 94.binary-tree-inorder-traversal
    base on recursive

---
+++ src/94.binary-tree-inorder-traversal.ts
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=94 lang=typescript
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.67%)
+ * Likes:    13159
+ * Dislikes: 745
+ * Total Accepted:    2.4M
+ * Total Submissions: 3.2M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the inorder traversal of its nodes'
+ * values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+import { TreeNode } from "../data_structure/module";
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+export function inorderTraversal(root: TreeNode | null): number[] {
+  if (!root) {
+    return [];
+  }
+  return [
+    ...inorderTraversal(root.left),
+    root.val,
+    ...inorderTraversal(root.right),
+  ];
+}
+// @lc code=end
