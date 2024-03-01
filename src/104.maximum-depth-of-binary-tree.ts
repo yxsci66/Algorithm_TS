8bce0c017cbab2d4075353538c5a66b5d432c933
yxsci66 <yxsci66@163.com>
2024-03-01 11:16:57 +0800

    feat add 104.maximum-depth-of-binary-tree
    base on recursive call

---
+++ src/104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=104 lang=typescript
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (74.93%)
+ * Likes:    12481
+ * Dislikes: 212
+ * Total Accepted:    2.9M
+ * Total Submissions: 3.9M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return its maximum depth.
+ *
+ * A binary tree's maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,null,2]
+ * Output: 2
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 10^4].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+import { TreeNode } from "../data_structure/module";
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+export function maxDepth(root: TreeNode | null): number {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(maxDepth(root?.left), maxDepth(root?.right)) + 1;
+}
+// @lc code=end
