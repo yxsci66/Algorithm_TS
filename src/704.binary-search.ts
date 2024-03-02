5bd615b03d65439e981f8dffce38c7904dafdb31
yxsci66 <yxsci66@163.com>
2024-03-02 22:21:51 +0800

    feat add 226 && 704

---
+++ src/704.binary-search.ts
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=704 lang=typescript
+ *
+ * [704] Binary Search
+ *
+ * https://leetcode.com/problems/binary-search/description/
+ *
+ * algorithms
+ * Easy (57.04%)
+ * Likes:    11470
+ * Dislikes: 234
+ * Total Accepted:    2.3M
+ * Total Submissions: 4M
+ * Testcase Example:  '[-1,0,3,5,9,12]\n9'
+ *
+ * Given an array of integers nums which is sorted in ascending order, and an
+ * integer target, write a function to search target in nums. If target exists,
+ * then return its index. Otherwise, return -1.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^4 < nums[i], target < 10^4
+ * All the integers in nums are unique.
+ * nums is sorted in ascending order.
+ *
+ *
+ */
+
+// @lc code=start
+export function search(nums: number[], target: number): number {
+  let [left, right] = [0, nums.length - 1];
+  let midIndex: number;
+  while (left <= right) {
+    midIndex = (left + right) >> 1;
+    if (nums[midIndex]! > target) {
+      right = midIndex - 1;
+    } else if (nums[midIndex]! < target) {
+      left = midIndex + 1;
+    } else {
+      return midIndex;
+    }
+  }
+  return -1;
+}
+// @lc code=end
